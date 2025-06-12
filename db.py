397cc757e457189ff3cbfb1f974c5ea39e291d44
Scott Steele <yttcs@msn.com>
2025-06-12 00:30:47 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlmodel import Session, create_engine
 
 db_name = "users"
-mysql_url = f"mysql+pymysql://root:xcr223@188.166.217.197:3306/{db_name}"
+mysql_url = f"mysql+pymysql://root: password@ipaddress:3306/{db_name}"
 engine = create_engine(mysql_url, echo=True)
 
 def get_session():
